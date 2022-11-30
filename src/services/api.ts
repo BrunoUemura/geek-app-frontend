d28ad30cee156798f97cb23a3fecd9a744948c10
BrunoUemura <bruno.h.uemura@outlook.com>
2022-11-30 20:29:30 -0300

    feat: adds proper env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = "https://geek-app-backend-serverless.vercel.app/api";
+const baseURL = import.meta.env.VITE_BACKEND_API || "http://localhost:3000/api";
 
 export const api = axios.create({ baseURL });
