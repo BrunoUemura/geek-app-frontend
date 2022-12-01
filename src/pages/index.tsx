fd71d1a16ac026f7b9061d537344a9984b5d8c4e
BrunoUemura <bruno.h.uemura@outlook.com>
2022-12-01 00:11:46 -0300

    feat: adds redirect in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import Router from "next/router";
 import { useEffect } from "react";
+import { useAuth } from "../hooks/useAuth";
 import { ROUTES } from "../routes";
 
 export default function Home() {
+  const { isAuthenticated } = useAuth();
+
+  const handleListsRedirect = () => Router.push(ROUTES.LIST);
   const handleSignInRedirect = () => Router.push(ROUTES.LOGIN);
+
+  if (isAuthenticated) {
+    return (
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <h1 className="text-xl mb-10">Go to your lists</h1>
+        <button
+          className="bg-black text-white p-2 rounded-lg"
+          onClick={handleListsRedirect}
+        >
+          Show Lists
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
